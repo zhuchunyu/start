e1549eddb89170cb0accbdc39fb69e2741123456
yuz <zhuchunyu_14a@163.com>
2017-10-14 22:57:18 +0800

    session

--- src/main/java/com/habage/service/GradeService.java
+++ src/main/java/com/habage/service/GradeService.java
@@ -1,7 +1,10 @@
 package com.habage.service;
 
+import com.alibaba.fastjson.JSON;
 import com.habage.mapper.GradeMapper;
 import com.habage.model.Grade;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,8 +17,19 @@ public class GradeService {
     @Autowired
     private GradeMapper gradeMapper;
 
+    @Autowired
+    private SqlSessionFactory sessionFactory;
+
     @Transactional
     public Grade getbyid_mapper(Integer id) {
+        SqlSession session = sessionFactory.openSession();
+        System.out.println(session);
+        Grade grade = session.selectOne("getbyid_xml_sql", 2);
+        System.out.println(JSON.toJSONString(grade));
+
+        session.close();
+
+
         return gradeMapper.selectByPrimaryKey(id);
     }
 
---
+++ src/test/java/com/habage/test/MybatisTest.java
@@ -0,0 +1,25 @@
+package com.habage.test;
+
+
+import com.habage.model.Grade;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.InputStream;
+
+public class MybatisTest {
+    public static void main(String[] args) throws Exception {
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+
+        SqlSession session = sqlSessionFactory.openSession();
+
+        Grade grade = session.selectOne("getbyid_xml_sql", 2);
+        System.out.println(grade.getGradeNm());
+
+        session.close();
+    }
+}
